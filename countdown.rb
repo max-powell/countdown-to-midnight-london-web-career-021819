64f44d47c4f8985067087be8eb902175cbc2df73
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 12:12:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,15 @@ def countdown(num)
   while num > 0
     puts "#{num} SECOND(S)!"
     num -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  count = num
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
     sleep(1)
   end
   "HAPPY NEW YEAR!"
